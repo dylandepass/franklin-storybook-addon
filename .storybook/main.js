9cb452221d6a01d3f44f9ca6a83e31fd0bca47b3
Dylan Depass <dylandepass@gmail.com>
2023-03-08 12:21:08 -0500

    fix: update storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,6 @@
 module.exports = {
   stories: ["../stories/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: ["../preset.js", "@storybook/addon-essentials"],
+  addons: ["../preset.js", "@storybook/addon-essentials", "@storybook/addon-mdx-gfm"],
   framework: {
     name: "@storybook/html-webpack5",
     options: {}
