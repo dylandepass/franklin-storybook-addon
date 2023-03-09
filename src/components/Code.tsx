b162693178caeeedf59ffda57c44573cf487cdd1
Dylan Depass <dylandepass@gmail.com>
2023-03-08 19:57:14 -0500

    fix: switching between panels

--- src/components/Code.tsx
+++ src/components/Code.tsx
@@ -1,27 +1,32 @@
 import React, { useState, useEffect } from 'react';
-import { useAddonState, useStorybookApi } from '@storybook/manager-api';
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import darkTheme from 'react-syntax-highlighter/dist/esm/styles/hljs/monokai';
 import prettier from 'prettier/standalone';
 import prettierHtml from 'prettier/parser-html';
-import { ADDON_ID } from '../constants';
-import { useChannel } from '@storybook/manager-api';
-import { STORY_RENDERED, STORY_CHANGED } from '@storybook/core-events'
+import { useChannel, useStorybookApi } from '@storybook/manager-api';
+import { STORY_RENDERED } from '@storybook/core-events'
 
 export const Code: React.FC = () => {
-  const [state, setState] = useAddonState(ADDON_ID, {
-    code: '',
+  const [code, setCode] = useState('');
+  const api = useStorybookApi();
+  const channel = api?.getChannel() || {};
+
+  useEffect(() => {
+    const rendered = (channel as any)?.data?.['franklin/block-rendered']?.[0] || 'No code found';
+    if(rendered.code) {
+      setCode(rendered.code);
+    }
   });
 
   useChannel({
-    ['franklin/block-rendered']: ({ code }) => setState((state) => ({ ...state, code })),
+    ['franklin/block-rendered']: ({ code }) => setCode(code ?? ''),
   });
 
   useChannel({
-    [STORY_RENDERED]: ({ code }) => setState((state) => ({ ...state, code: '' })),
+    [STORY_RENDERED]: ({ code }) => setCode(code ?? ''),
   });
 
-  const formatted = prettier.format(`${state.code}`, {
+  const formatted = prettier.format(`${code}`, {
     parser: 'html',
     plugins: [prettierHtml],
     htmlWhitespaceSensitivity: 'ignore',
